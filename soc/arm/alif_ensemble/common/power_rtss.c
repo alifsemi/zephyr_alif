30a86d53ca556e8786e9bd7f344581d4ac5b38e1
Sudhir Sreedharan <sudhir@alifsemi.com>
2025-05-15 17:22:42 +0530

    soc: arm: alif_ensemble: support s2ram
    Suspend to RAM support is added
    
    Signed-off-by: Sudhir Sreedharan <sudhir@alifsemi.com>

--- soc/arm/alif_ensemble/common/power_rtss.c
+++ soc/arm/alif_ensemble/common/power_rtss.c
@@ -6,11 +6,38 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/pm/pm.h>
+#if defined(CONFIG_PM_S2RAM)
+#include <zephyr/arch/common/pm_s2ram.h>
+#include <zephyr/drivers/timer/system_timer.h>
+#endif
+#include <zephyr/logging/log.h>
 #include <cmsis_core.h>
 #include <pm_rtss.h>
 
-#include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(soc, CONFIG_SOC_LOG_LEVEL);
+
+/* WICCONTROL register */
+#if defined(CONFIG_RTSS_HP)
+#define WICCONTROL                  (AON_RTSS_HP_CTRL)
+#elif defined(CONFIG_RTSS_HE)
+#define WICCONTROL                  (AON_RTSS_HE_CTRL)
+#else
+#error "Invalid CPU"
+#endif
+
+/* WIC bit positions in WICCONTROL */
+/*!< WICCONTROL: bit 8 (architecture dependent) */
+#define WICCONTROL_WIC_Pos (8U)
+#define WICCONTROL_WIC_Msk (1U << WICCONTROL_WIC_Pos)
+
+#if defined(CONFIG_PM_S2RAM)
+static int pm_suspend_to_ram(void)
+{
+	pm_core_enter_deep_sleep_request_subsys_off();
+
+	return 0;
+}
+#endif
 
 /* Handle PM specific states */
 __weak void pm_state_set(enum pm_state state, uint8_t substate_id)
@@ -18,6 +45,26 @@ __weak void pm_state_set(enum pm_state state, uint8_t substate_id)
 	ARG_UNUSED(substate_id);
 
 	switch (state) {
+#if defined(CONFIG_PM_S2RAM)
+	case PM_STATE_SUSPEND_TO_RAM:
+		__disable_irq();
+		__set_BASEPRI(0);
+
+		/* Save context and enter Standby mode */
+		pm_s2ram_save_ext_regs();
+
+		arch_pm_s2ram_suspend(pm_suspend_to_ram);
+
+		/* Restore context */
+		pm_s2ram_restore_ext_regs();
+
+		/* Clear the WIC Sleep */
+		sys_write32(_VAL2FLD(WICCONTROL_WIC, 0), WICCONTROL);
+
+		/* Restore system clock as soon as we exit standby mode */
+		sys_clock_idle_exit();
+		break;
+#endif
 	case PM_STATE_SOFT_OFF:
 		__disable_irq();
 		__set_BASEPRI(0);
@@ -35,6 +82,11 @@ __weak void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
 	ARG_UNUSED(substate_id);
 
 	switch (state) {
+#if defined(CONFIG_PM_S2RAM)
+	case PM_STATE_SUSPEND_TO_RAM:
+		__enable_irq();
+		break;
+#endif
 	case PM_STATE_SOFT_OFF:
 		__enable_irq();
 		break;
