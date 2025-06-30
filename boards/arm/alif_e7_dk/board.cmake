8d3f7d3bc1f1ebeddc3d7a9ebebf7d13d7e82be4
Marcell Varga <marcell.varga@arm.com>
2025-06-30 16:44:26 +0200

    [add] jlink runner config added for multicore debug
    Signed-off-by: Marcell Varga <marcell.varga@arm.com>

--- boards/arm/alif_e7_dk/board.cmake
+++ boards/arm/alif_e7_dk/board.cmake
@@ -1,3 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 include(${ZEPHYR_BASE}/boards/common/alif_flash.board.cmake)
+
+if(CONFIG_SOC_E7_DK_RTSS_HP)
+  board_runner_args(jlink "--device=Cortex-M55" "--speed=55000" "--gdb-port=2341")
+elseif(CONFIG_SOC_E7_DK_RTSS_HE)
+  board_runner_args(jlink "--device=Cortex-M55" "--speed=55000" "--gdb-port=2331")
+endif()
+
+set(BOARD_FLASH_RUNNER jlink)
+set(BOARD_DEBUG_RUNNER jlink)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
