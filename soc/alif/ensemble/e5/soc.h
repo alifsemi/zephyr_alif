510de1b4c07d2de37e3bebaf931cd6e043965941
Prabhakar Kumar <prabhakar.kumar@alifsemi.com>
2026-02-09 17:20:27 +0530

    boards: alif: alif_e5_dk: add ae512f80f55d5xx support
    Add support for the ae512f80f55d5xx series to the alif_e5_dk board.
    
    Signed-off-by: Prabhakar Kumar <prabhakar.kumar@alifsemi.com>

---
+++ soc/alif/ensemble/e5/soc.h
@@ -0,0 +1,11 @@
+/* Copyright (c) 2026 Alif Semiconductor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef SOC_ALIF_ENSEMBLE_E5_SOC_H_
+#define SOC_ALIF_ENSEMBLE_E5_SOC_H_
+
+#include <cmsis_core_m_defaults.h>
+
+#endif /* SOC_ALIF_ENSEMBLE_E5_SOC_H_ */
