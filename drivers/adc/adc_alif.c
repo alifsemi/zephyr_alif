3e60748fdaab98b3cda2dc5570b7a99d42974ead
Prabhakar Kumar <prabhakar.kumar@alifsemi.com>
2026-09-03 20:32:47 +0530

    drivers: adc: comparator: fix analog macro redefinition warnings
    The HAL already defines ANA_PERIPH_LDO_CONT and ANA_PERIPH_BG_CONT.
    Remove the duplicate defines from the ADC and comparator drivers.
    
    Signed-off-by: Prabhakar Kumar <prabhakar.kumar@alifsemi.com>

--- drivers/adc/adc_alif.c
+++ drivers/adc/adc_alif.c
@@ -166,8 +166,6 @@ enum ADC_INSTANCE {
 #define ADC_VREF_BUF_RDIV_EN (0x0U << 16)
 #define ADC_VREF_BUF_EN      (0x1U << 15)
 #define ADC_VREF_CONT        (0x10U << 10)
-#define ANA_PERIPH_LDO_CONT  (0xAU << 6)
-#define ANA_PERIPH_BG_CONT   (0xAU << 1)
 
 #ifdef CONFIG_ANALOG_ALIASING
 /* ADC reg1 position macro */
